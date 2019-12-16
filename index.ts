dea8a4ba4cd6ec83211653eaa7ac2057c242f8f1
meronbrouwer <meron.brouwer@gmail.com>
2019-12-16 11:18:56 +0100

    Encapsulated FizzBuzz in a function

--- index.ts
+++ index.ts
@@ -1,20 +1,24 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
+const appDiv: HTMLElement = document.getElementById("app");
 appDiv.innerHTML = `<h1>FizzBuzz</h1>`;
 
-for(let i:number = 0; i < 100 ; i++){
-  let fizzBuzz : string = "";
-  if(i % 3 == 0){
-    fizzBuzz += "Fizz";
+function doFizzBuzz(): void {
+  for (let i: number = 0; i < 100; i++) {
+    let fizzBuzz: string = "";
+    if (i % 3 == 0) {
+      fizzBuzz += "Fizz";
+    }
+    if (i % 5 == 0) {
+      fizzBuzz += "Buzz";
+    }
+    if (fizzBuzz == "") {
+      fizzBuzz = i.toString();
+    }
+    console.log(fizzBuzz);
   }
-  if(i % 5 == 0){
-    fizzBuzz += "Buzz";
-  }
-  if (fizzBuzz == ""){
-    fizzBuzz = i.toString();
-  }
-  console.log(fizzBuzz);
 }
+
+doFizzBuzz();
